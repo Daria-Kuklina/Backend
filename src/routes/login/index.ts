1eb63c2e48e617298374fd8d481c56f4e68c87d7
Frogget <happylittlefrog@outlook.com>
2021-11-08 20:02:31 +0300

    backend update

---
+++ src/routes/login/index.ts
@@ -0,0 +1,24 @@
+import getUsersList, {swGetUser} from './login-get.route'
+import createTheUser, {swPostUser} from './login-post.route'
+
+const express = require('express')
+// here the our swagger info
+export const swLoginRouter = {
+  "/login": {
+    "get": {
+      ...swGetUser
+    },
+    "post": {
+      ...swPostUser
+    }
+  }
+}
+// here the routes
+const router = express()
+router.get('/', function (req: any, res: any) {
+  getUsersList(req, res)
+})
+router.post('/', function (req: any, res: any) {
+  createTheUser(req, res)
+})
+export default router
