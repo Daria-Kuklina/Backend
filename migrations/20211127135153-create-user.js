ed7d9b3f512582ad2b23a5d12606bdbc8820d871
Daria-Kuklina <lDaria.Kl@yandex.ru>
2021-11-29 18:55:04 +0300

    "ky"

---
+++ migrations/20211127135153-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      login: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      mail: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  down: async (queryInterface, DataTypes) => {
+    await queryInterface.dropTable('users');
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import swDocument from './swagger.def'
 
+const { sequelize, User } = require("../models");
+
 const express = require("express"),
   http = require("http"),
   swaggerUI = require('swagger-ui-express')
@@ -15,4 +17,85 @@ app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swDocument))
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  //sequelize.authenticate()
+  sequelize.sync()
+  console.log('Database Connected!')
+});
+
+
+app.post("/users", async (req: any, res: any) => {
+  const { login, password, mail} = req.body;
+  try{
+    const user = await User.create({login, password, mail})
+    return res.json(user)
+  } catch(err){
+    console.log(err)
+    return res.status(500).json(err)
+
+  }
+});
+
+app.get("/users", async (_: any, res: any) => {
+  try {
+    const users = await User.findAll();
+    return res.json(users);
+  } catch (err) {
+    console.log(err);
+    return res.json(err);
+  }
+  
+});
+
+app.get('/users/:id', async (req: any, res: any) =>{
+  const id = req.params.uuid
+  try{
+    const user = await User.findOne({
+      were: { id }
+    })
+    return res.json(user)
+  } 
+  catch(err){
+    console.log(err)
+    return res.status(500).json({eror:'Something went wrong'})
+  }
+
+});
+
+app.delete('/users/:id', async (req: any, res: any) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findOne({
+      where: { id },
+    });
+
+    await user.destroy();
+    return res.status(204).json({ message: "User deleted successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.json(err);
+  }
+});
+
+app.put("/users/:id", async (req: any, res: any) => {
+  const id = req.params.id;
+  const { login, password, mail } = req.body;
+
+  try {
+    const user = await User.findOne({
+      where: { id },
+    });
+
+
+    user.login = login;
+    user.password = password;
+    user.mail = mail;
+
+    await user.save();
+
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.json(err);
+  }
 });
